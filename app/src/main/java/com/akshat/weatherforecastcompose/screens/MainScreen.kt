359ef8e627ee6a90c647b3e9c408f3200303e6e7
Akshat <akshat@Akshats-MacBook-Pro.local>
2024-08-20 16:01:30 -0400

    changes for first screen

--- app/src/main/java/com/akshat/weatherforecastcompose/screens/MainScreen.kt
+++ app/src/main/java/com/akshat/weatherforecastcompose/screens/MainScreen.kt
@@ -1,20 +1,44 @@
 package com.akshat.weatherforecastcompose.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.akshat.weatherforecastcompose.data.DataOrException
 import com.akshat.weatherforecastcompose.model.Weather
+import com.akshat.weatherforecastcompose.model.WeatherItem
+import com.akshat.weatherforecastcompose.utils.formatDate
+import com.akshat.weatherforecastcompose.utils.formatDecimals
+import com.akshat.weatherforecastcompose.widgets.HumidityWindPressureRow
+import com.akshat.weatherforecastcompose.widgets.SunsetSunriseRow
 import com.akshat.weatherforecastcompose.widgets.WeatherAppBar
+import com.akshat.weatherforecastcompose.widgets.WeatherDetailRow
+import com.akshat.weatherforecastcompose.widgets.WeatherStateImage
 
 
 @Composable
@@ -40,16 +64,20 @@ fun MainScreen(
 @Composable
 fun MainScaffold(weather: Weather, navController: NavController) {
 
-    Scaffold (topBar = {
-        WeatherAppBar(title = weather.city.name + ", ${weather.city.country}",
+    Scaffold(topBar = {
+        WeatherAppBar(
+            title = weather.city.name + ", ${weather.city.country}",
             icon = Icons.Default.ArrowBack,
             navController = navController,
-            elevation = 7.dp){
+            elevation = 7.dp
+        ) {
 
         }
 
-    }) { _ ->
-        MainContent(data = weather)
+    }) { it ->
+        Surface(modifier = Modifier.padding(top = it.calculateTopPadding())) {
+            MainContent(data = weather)
+        }
     }
 
 
@@ -57,9 +85,84 @@ fun MainScaffold(weather: Weather, navController: NavController) {
 
 @Composable
 fun MainContent(data: Weather) {
-    Text(modifier = Modifier.padding(
-        top = 9.dp,
-        bottom = 8.dp
-    ),text = data.city.name)
+
+    val weatherItem = data.list[0]
+    val imageUrl = "https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}.png"
+
+    Column(
+        Modifier
+            .padding(
+                start = 4.dp,
+                top = 9.dp,
+                end = 4.dp,
+                bottom = 4.dp
+            )
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = formatDate(weatherItem.dt), // Wed Nov 17
+            style = MaterialTheme.typography.bodyLarge,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.padding(6.dp)
+        )
+
+        Surface(
+            modifier = Modifier
+                .padding(4.dp)
+                .size(200.dp),
+            shape = CircleShape,
+            color = Color(0xFFFFC400)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                WeatherStateImage(imageUrl = imageUrl)
+                Text(
+                    text = formatDecimals(weatherItem.temp.day) + "º",
+                    style = MaterialTheme.typography.headlineSmall,
+                    fontWeight = FontWeight.ExtraBold
+                )
+                Text(text = weatherItem.weather[0].main, fontStyle = FontStyle.Italic)
+
+
+            }
+
+        }
+        HumidityWindPressureRow(weather = data.list[0])
+        Divider()
+        SunsetSunriseRow(weather = data.list[0])
+        Text(
+            text = "This week",
+            style = MaterialTheme.typography.bodyLarge,
+            fontWeight = FontWeight.Bold
+        )
+
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            color = Color(0xFFEEF1EF),
+            shape = RoundedCornerShape(14.dp)
+        ) {
+            LazyColumn(
+                modifier = Modifier.padding(2.dp),
+                contentPadding = PaddingValues(1.dp)
+            ) {
+                items(items = data.list) { item: WeatherItem ->
+                    //     Text(item.temp.max.toString())
+                    WeatherDetailRow(weather = item)
+
+                }
+            }
+
+        }
+    }
+
 
 }
+
+
